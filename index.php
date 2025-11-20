b15a14923a620d14eb18a3e7c9164413537a0da6
jhonatan <jhonatannfarfan@gmail.com>
2025-11-20 09:13:41 -0500

    12

--- index.php
+++ index.php
@@ -485,8 +485,8 @@ if (!isset($pdo)) {
             function displayMototaxiInfo(mototaxi, fuente) {
                 const fuenteBadge = fuente === 'API_EXTERNA' ? 
                     '<span class="badge bg-success">API Externa</span>' : 
-                    fuente === 'BD_LOCAL' ? 
-                    '<span class="badge bg-info">Base Local</span>' :
+                    fuente === 'VEGETA' ? 
+                    '<span class="badge bg-info">información del Nº Asignado a la Mototaxi</span>' :
                     '<span class="badge bg-warning">Datos de Prueba</span>';
 
                 const infoHtml = `
